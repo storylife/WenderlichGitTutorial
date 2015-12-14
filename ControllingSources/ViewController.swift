b52fcc3626a4b25f4c1a914bef8ad31a80648401
Olli Brzoska <oliver.brzoska@gmail.com>
2015-12-14 14:34:39 +0100

    New project setup.

---
+++ ControllingSources/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ControllingSources
+//
+//  Created by Olli Brzoska on 14.12.15.
+//  Copyright © 2015 Goodsquirrel Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ControllingSourcesTests/ControllingSourcesTests.swift
@@ -0,0 +1,36 @@
+//
+//  ControllingSourcesTests.swift
+//  ControllingSourcesTests
+//
+//  Created by Olli Brzoska on 14.12.15.
+//  Copyright © 2015 Goodsquirrel Apps. All rights reserved.
+//
+
+import XCTest
+@testable import ControllingSources
+
+class ControllingSourcesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
